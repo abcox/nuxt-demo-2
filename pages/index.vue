6f90f1a15b0f2bbfbec82ee6f23bd42b0506a64b
Adam Cox <adam@adammcox.net>
2021-04-03 16:22:44 -0700

    start, add menu

---
+++ pages/index.vue
@@ -0,0 +1,109 @@
+<template>
+  <div style="width: 256px">
+    <a-button type="primary" style="margin-bottom: 16px" @click="toggleCollapsed">
+      <a-icon :type="collapsed ? 'menu-unfold' : 'menu-fold'" />
+    </a-button>
+    <a-menu
+      :default-selected-keys="['1']"
+      :default-open-keys="['sub1']"
+      mode="inline"
+      theme="light"
+      :inline-collapsed="collapsed"
+    >
+      <a-menu-item key="1">
+        <a-icon type="pie-chart" />
+        <span>Option 1</span>
+      </a-menu-item>
+      <a-menu-item key="2">
+        <a-icon type="desktop" />
+        <span>Option 2</span>
+      </a-menu-item>
+      <a-menu-item key="3">
+        <a-icon type="inbox" />
+        <span>Option 3</span>
+      </a-menu-item>
+      <a-sub-menu key="sub1">
+        <span slot="title"><a-icon type="mail" /><span>Navigation One</span></span>
+        <a-menu-item key="5">
+          Option 5
+        </a-menu-item>
+        <a-menu-item key="6">
+          Option 6
+        </a-menu-item>
+        <a-menu-item key="7">
+          Option 7
+        </a-menu-item>
+        <a-menu-item key="8">
+          Option 8
+        </a-menu-item>
+      </a-sub-menu>
+      <a-sub-menu key="sub2">
+        <span slot="title"><a-icon type="appstore" /><span>Navigation Two</span></span>
+        <a-menu-item key="9">
+          Option 9
+        </a-menu-item>
+        <a-menu-item key="10">
+          Option 10
+        </a-menu-item>
+        <a-sub-menu key="sub3" title="Submenu">
+          <a-menu-item key="11">
+            Option 11
+          </a-menu-item>
+          <a-menu-item key="12">
+            Option 12
+          </a-menu-item>
+        </a-sub-menu>
+      </a-sub-menu>
+    </a-menu>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component } from 'nuxt-property-decorator'
+import Vue from 'vue'
+
+@Component({
+  // nothing
+})
+export default class IndexPage extends Vue {
+
+  collapsed = false;
+
+  toggleCollapsed() {
+    this.collapsed = !this.collapsed;
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
